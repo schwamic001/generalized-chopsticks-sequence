883758d387148a552681b185572e4fe8a6125384
schwadan001 <schwadan001@gmail.com>
2020-02-23 11:13:52 -0600

    add graphs

--- generate_polar.py
+++ generate_polar.py
@@ -6,6 +6,11 @@ from optparse import OptionParser
 
 
 def save_pattern(seq, end):
+    """ Saves a polar graph of the GCS sequence, up to a specified stopping point in the sequence
+
+    @param seq (list[int]) - the generalized chopstick sequence
+    @param end (int) - only GCS numbers less than or equal to `end` are included in the graph
+    """
     size = round(log(end)) * 2
     fig = plt.figure(figsize=(size, size))
     trunc_seq = [n for n in seq if n <= end]
@@ -20,8 +25,11 @@ with open(SEQ_FILE) as f:
     seq = [int(line.strip()) for line in f.readlines() if line != '']
     max_calced = max(seq)
 
+# set the limit on how much of the GCS sequence we want in our graph
+# if user does not specify a limit, default to the largest number we've checked
 parser = OptionParser()
 parser.add_option('--end', default=str(max_calced))
 (options, args) = parser.parse_args()
 
+# save polar graph to file
 save_pattern(seq, int(options.end))
--- generate_sequence.py
+++ generate_sequence.py
@@ -3,13 +3,24 @@ from optparse import OptionParser
 
 
 def log(msg):
+    """ Logs a message with the timestamp of when it occurred
+
+    @param msg (string) - message to be logged
+    """
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     print("{0} {1}".format(current_time, msg))
 
 
-def p1_win(start, n):
-    (p1, p2) = (start, start)
+def p1_win(i, n):
+    """ Returns whether or not the first player to get hit wins the game
+
+    @param i (int) - number of fingers each player starts with
+    @param n (int) - total number of fingers on a hand
+
+    @return (boolean)
+    """
+    (p1, p2) = (i, i)
     while p1 % n != 0 and p2 % n != 0:
         p1 = (p1 + p2) % n
         p2 = (p1 + p2) % n
@@ -17,6 +28,15 @@ def p1_win(start, n):
 
 
 def is_seq_member(n):
+    """
+    Returns whether or not a number `n` is a member of the generalized chopstick sequence.
+    A number `n` is a sequence member iff the same player either wins or loses every round
+    for all values of `i` (0 < i < n) where `i` is the number of fingers each player starts with
+
+    @param n (int) - total number of fingers on a hand
+
+    @return (boolean)
+    """
     if n < 2:
         return False
     first_win = p1_win(1, n)
@@ -26,9 +46,11 @@ def is_seq_member(n):
     return True
 
 
+# files used for storing and calculating the generalized chopstick sequence
 SEQ_FILE = 'gcs.txt'
 MAX_FILE = 'max_calc.txt'
 
+# determine where we need to resume calculating the sequence from
 try:
     with open(SEQ_FILE) as f:
         seq = [int(line.strip()) for line in f.readlines() if line != '']
@@ -47,12 +69,14 @@ except FileNotFoundError:
     with open(MAX_FILE, 'w') as f:
         f.write(str(max_calced))
 
+# determine when to stop calculating the sequence (user input or default)
 parser = OptionParser()
 parser.add_option('--end', default=str(max_calced + 5000))
 (options, args) = parser.parse_args()
 
 end = int(options.end)
 
+# generate GCS sequence numbers and save them to a file
 log('Processing until {0}'.format(end))
 
 for n in range(max_calced + 1, end + 1):
